a4cf852ad0e903c7137fcec7d9642554f4d3a8eb
andyfeely <andyfeely@gmail.com>
2019-11-24 20:29:01 +0000

    [master]  - Added primitive strings for colors in theme components

--- src/components/atoms/MatThemeComponent.vue
+++ src/components/atoms/MatThemeComponent.vue
@@ -6,35 +6,41 @@ import theme from '@materiajs/theme';
   props: {
     initTheme: {
       type: Function,
-      default: (props, setDefaults = true) => ({
-        // eslint-disable-next-line
-        '--color': props.color ? (props.color.includes('#') ? props.color : `var(--${props.color})`) : undefined,
-        '--gradient-color': props.gradient ? `var(--${props.gradient})` : undefined,
-        '--gradient-direction': props.gradientDirection || undefined,
-        '--text-color': props.color
-          ? theme.computeTextColor(
-            (props.color.includes('#') ? props.color : document.documentElement.style.getPropertyValue(`--${props.color}`)),
-            true,
-          )
-          : undefined,
-        '--round': props.round ? `${parseInt(props.round, 10) * 5}px` : undefined,
-        padding: props.padding,
-        margin: props.margin,
-        display: props.display,
-        cursor: props.cursor,
-        height: props.height,
-        width: props.width,
-        'max-width': props.maxWidth,
-        'z-index': props.zIndex,
-        ...(setDefaults ? {
-          background: props.color ? `linear-gradient(
+      default: (props, setDefaults = true) => {
+        const isHash = props.color && props.color.includes('#');
+        const isGradientHash = props.gradient && props.gradient.includes('#');
+        const primaryColor = isHash ? props.color : `var(--${props.color})`;
+        const gradientColor = isGradientHash ? props.gradient : `var(--${props.gradient})`;
+        return {
+          // eslint-disable-next-line
+          '--color': primaryColor || undefined,
+          '--gradient-color': gradientColor,
+          '--gradient-direction': props.gradientDirection || undefined,
+          '--text-color': props.color
+            ? theme.computeTextColor(
+              (isHash ? props.color : document.documentElement.style.getPropertyValue(`--${props.color}`)),
+              true,
+            )
+            : undefined,
+          '--round': props.round ? `${parseInt(props.round, 10) * 5}px` : undefined,
+          padding: props.padding,
+          margin: props.margin,
+          display: props.display,
+          cursor: props.cursor,
+          height: props.height,
+          width: props.width,
+          'max-width': props.maxWidth,
+          'z-index': props.zIndex,
+          ...(setDefaults ? {
+            background: props.color ? `linear-gradient(
               var(--gradient-direction, to bottom),
               var(--color),
               ${props.gradient ? 'var(--gradient-color)' : 'var(--color)'}
             )` : undefined,
-          color: 'var(--text-color, var(--default-text))',
-        } : {}),
-      }),
+            color: 'var(--text-color, var(--default-text))',
+          } : {}),
+        };
+      },
     },
   },
 })
